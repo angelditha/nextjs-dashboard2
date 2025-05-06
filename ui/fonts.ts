c33430b6b806c012e218e44e08116d8f2985620b
angelditha <dithaangelditha@gmail.com>
2025-05-07 03:11:10 +0800

    Initial commit

---
+++ ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Lusitana } from 'next/font/google'; // or wherever you're importing it from
+
+export const lusitana = Lusitana({
+  subsets: ['latin'], // or appropriate subsets
+  weight: ['400', '700'], // or appropriate weights
+});
